7429931aecd027ed945294043ae92c329b14d7c0
Adam Scott BSU <sxottcobolt@gmail.com>
2025-05-24 21:02:22 +0400

    Add files via upload

---
+++ Activity_2_2025_05_24_17_00_45/sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+  createCanvas(800, 800);
+  background(0);
+  noFill();
+  stroke(255);
+  strokeWeight(50); // Thickness of the ring
+  ellipse(width / 2, height / 2, 500, 500); // Centered ring
+}
