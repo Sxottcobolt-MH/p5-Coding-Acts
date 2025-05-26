07e9c2ae6cc1f71d898876ac2d8f3270c9f4f9d0
Adam Scott BSU <sxottcobolt@gmail.com>
2025-05-26 19:30:26 +0400

    Delete Activity_2_2025_05_24_17_00_45 directory

--- Activity_2_2025_05_24_17_00_45/sketch.js
+++
@@ -1,8 +0,0 @@
-function setup() {
-  createCanvas(800, 800);
-  background(0);
-  noFill();
-  stroke(255);
-  strokeWeight(50); // Thickness of the ring
-  ellipse(width / 2, height / 2, 500, 500); // Centered ring
-}
